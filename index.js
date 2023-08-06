9d4514f7199a74cac334ace3b67d4bcb540e222d
Steven Cheng <a09930185@gmail.com>
2023-08-06 19:57:39 +0800

    docs(package): add vscode conventional commit

---
+++ index.js
@@ -0,0 +1,10 @@
+/** build a simple server by using javascript */
+const http = require('http');
+const PORT = 8080;
+
+http.createServer((req, res) => {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('Hello World');
+}).listen(PORT,(err, res) => {
+    console.log('server is running');
+});
